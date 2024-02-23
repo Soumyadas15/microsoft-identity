0e3260c26ada63bdfb3db943463f8719b0d79ef6
promact <saymyadas2017@gmail.com>
2024-02-23 16:39:58 +0530

    4. Padding bug fix

--- app/components/Box.tsx
+++ app/components/Box.tsx
@@ -15,9 +15,7 @@ export const Box = ({
 }: BoxrProps) => {
 
     return (
-        <div className={`w-[15rem] ${height} bg-white rounded-lg flex flex-col items-center justify-between p-5shadow-lg
-                
-            `}
+        <div className={`w-[15rem] ${height} bg-white rounded-lg flex flex-col items-center justify-between p-5 shadow-lg`}
         >
             {children}
         </div>
